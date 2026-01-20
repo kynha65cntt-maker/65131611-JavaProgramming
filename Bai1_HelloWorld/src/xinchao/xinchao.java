8fa63ea1a464b1fff48d4a7192ac7156afe8aa35
kynha65cntt-maker <ky.nha.65cntt@ntu.edu.vn>
2026-01-20 22:19:42 +0700

    lap trinh java
    lap

---
+++ Bai1_HelloWorld/src/xinchao/xinchao.java
@@ -0,0 +1,12 @@
+package xinchao;
+
+public class xinchao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("Xin chao toi la sv 65CNTT2 - NTU, toi dang hoc LT Java");
+		System.out.print("Hi vong hoc lap trinh sẽ vui");
+	}
+
+}
