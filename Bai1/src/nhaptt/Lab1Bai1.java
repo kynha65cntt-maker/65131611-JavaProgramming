c1b447936124d61e6d77a6c7e9e04df8540e4caa
kynha65cntt-maker <ky.nha.65cntt@ntu.edu.vn>
2026-01-25 21:03:39 +0700

    Lab1bai1

---
+++ Bai1/src/nhaptt/Lab1Bai1.java
@@ -0,0 +1,15 @@
+package nhaptt;
+
+public class Lab1Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Họ và tên");
+		String hoTen = scanner.nextline();
+		System.out.print("Điểm TB: ");
+		double diemTB=scanner.nextDouble();
+		System.out.printf("%s %f điểm", hoTen, diemTB);
+	}
+
+}
