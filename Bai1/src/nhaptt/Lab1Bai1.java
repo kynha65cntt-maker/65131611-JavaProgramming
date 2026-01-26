2602894a5c9a35caa6a0a9d7d8984985035fda61
kynha65cntt-maker <ky.nha.65cntt@ntu.edu.vn>
2026-01-26 11:28:21 +0700

    new contest

--- Bai1/src/nhaptt/Lab1Bai1.java
+++ Bai1/src/nhaptt/Lab1Bai1.java
@@ -1,14 +1,16 @@
 package nhaptt;
 
+import java.util.Scanner;
+
 public class Lab1Bai1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Họ và tên");
-		String hoTen = scanner.nextline();
+		System.out.print("Họ và tên: ");
+		String hoTen = scanner.nextLine();
 		System.out.print("Điểm TB: ");
-		double diemTB=scanner.nextDouble();
+		double diemTB = scanner.nextDouble();
 		System.out.printf("%s %f điểm", hoTen, diemTB);
 	}
 
---
+++ lab1_2/src/lab1_2/Lab1Bai2.java
@@ -0,0 +1,16 @@
+package lab1_2;
+
+import java.util.Scanner;
+
+public class Lab1Bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhap vao chieu dai va rong ");
+		int dai = scanner.nextInt();
+		int rong = scanner.nextInt();
+		System.out.printf("Chu vi cua HCN: "+(dai+rong)*2);
+	}
+
+}
